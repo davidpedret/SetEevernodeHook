77006a3300cf7789872e783f284c34c1fc32f5a8
David Pedret Lopez <96491498+davidpedret@users.noreply.github.com>
2025-12-17 00:23:08 +0100

    Improve hook CLI prompt handling

--- evernode_hook_set.ts
+++ evernode_hook_set.ts
@@ -1,10 +1,4 @@
-import {
-  Client,
-  Wallet,
-  Payment,
-  SetHookFlags,
-  TransactionMetadata,
-} from '@transia/xrpl';
+import { Client, Wallet, SetHookFlags } from '@transia/xrpl'
 import {
   createHookPayload,
   setHooksV3,
@@ -12,91 +6,90 @@ import {
   iHookParamEntry,
   iHookParamName,
   iHookParamValue,
-  Xrpld,
-  ExecutionUtility,
-} from '@transia/hooks-toolkit';
-import {
-  xrpAddressToHex,
-} from '@transia/hooks-toolkit/dist/npm/src/libs/binary-models'
+} from '@transia/hooks-toolkit'
+import { xrpAddressToHex } from '@transia/hooks-toolkit/dist/npm/src/libs/binary-models'
 
-import * as xrpl from 'xrpl';
-import * as readline from "readline";
+import * as readline from 'readline'
+
+const askQuestion = (rl: readline.Interface, prompt: string) => {
+  return new Promise<string>((resolve) => rl.question(prompt, resolve))
+}
 
 export async function main(): Promise<void> {
+  const serverUrl = 'wss://xahau.network'
+  const client = new Client(serverUrl)
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
   try {
-    const serverUrl = 'wss://xahau.network';
-    const client = new Client(serverUrl);
-    await client.connect();
-    client.networkID = await client.getNetworkID();
+    await client.connect()
+    client.networkID = await client.getNetworkID()
 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+    const destinationAccount = await askQuestion(
+      rl,
+      'Please input your Desired destination account to forward EVR to:',
+    )
 
-    //Get Destination account
-    let detinationAccount = "_"
-    rl.question('Please input your Desired destination account to forward EVR to:', (answer) => {
-        detinationAccount = answer;
-    });
-    while (detinationAccount == "_") { await new Promise(r => setTimeout(r, 100))  }
-
-    let destinationID;
+    let destinationID
     try {
-        destinationID = xrpAddressToHex(detinationAccount)
+      destinationID = xrpAddressToHex(destinationAccount)
     } catch (error) {
-      console.error("Cant decode Account ID from given destination rAddress, please make sure to use proper destination address", error);
-      process.exit(1);
+      console.error(
+        'Cant decode Account ID from given destination rAddress, please make sure to use proper destination address',
+        error,
+      )
+      return
     }
-    console.log('Detination Account to send EVR to:' + detinationAccount);
+    console.log('Destination Account to send EVR to:' + destinationAccount)
 
-    let seed = "_"
-    rl.question('Please input your Host secret[seed]:', (answer) => {
-        seed = answer;
-    });
-    while (seed == "_") { await new Promise(r => setTimeout(r, 100))  }
+    const seed = await askQuestion(rl, 'Please input your Host secret[seed]:')
 
-    let consent = "_"
-    rl.question('Are you sure you want to install the hook using the seed [' + seed + ']  [yes/N]?', (answer) => {
-        consent = answer;
-        rl.close();
-    });
-    while (consent == "_") { await new Promise(r => setTimeout(r, 100))  }
+    const consent = await askQuestion(
+      rl,
+      `Are you sure you want to install the hook using the seed [${seed}]  [yes/N]?`,
+    )
 
-    if(consent.toLowerCase() != 'yes')
-    {
-       console.log('BA BYE');
-       process.exit(1);
+    if (consent.toLowerCase() !== 'yes') {
+      console.log('BA BYE')
+      return
     }
-    
-    const myWallet = Wallet.fromSeed(seed);
 
-    console.log("Creating hook payload...");
+    const myWallet = Wallet.fromSeed(seed)
 
-    const hookparam = new iHookParamEntry(new iHookParamName('A'),new iHookParamValue(destinationID, true))
-    const hookPayload = createHookPayload({version:0, createFile:'redirect', namespace:'redirect', flags:SetHookFlags.hsfOverride, hookOnArray:['Payment'], hookParams: [hookparam.toXrpl()],});
+    console.log('Creating hook payload...')
 
-    console.log("Generated hook payload:", hookPayload);
+    const hookparam = new iHookParamEntry(new iHookParamName('A'), new iHookParamValue(destinationID, true))
+    const hookPayload = createHookPayload({
+      version: 0,
+      createFile: 'redirect',
+      namespace: 'redirect',
+      flags: SetHookFlags.hsfOverride,
+      hookOnArray: ['Payment'],
+      hookParams: [hookparam.toXrpl()],
+    })
 
+    console.log('Generated hook payload:', hookPayload)
 
     try {
-      console.log("Setting hook...");
+      console.log('Setting hook...')
       await setHooksV3({
         client: client,
         seed: myWallet.seed,
         hooks: [{ Hook: hookPayload }],
-      } as SetHookParams);
-      console.log("Hook set successfully.");
+      } as SetHookParams)
+      console.log('Hook set successfully.')
     } catch (error) {
-      console.error("Error setting hook:", error);
+      console.error('Error setting hook:', error)
     }
-
-
-    await client.disconnect();
-    console.log("Disconnected from server.");
   } catch (error) {
-    console.error("Error occurred:", error);
+    console.error('Error occurred:', error)
+  } finally {
+    rl.close()
+    await client.disconnect()
+    console.log('Disconnected from server.')
   }
 }
 
-main();
+main()
